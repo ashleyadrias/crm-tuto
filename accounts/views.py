3af99c2973816ebfb76f1c6e7c704b62c83486f4
ashleyadrias <agadrias@gmail.com>
2020-03-22 15:16:28 -0700

    added delete and place order

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
+from .forms import OrderForm
 
 def home(request):
 	orders= Order.objects.all()
@@ -36,3 +38,52 @@ def customer(request, pk):
 				'orders': orders,
 				'order_count': order_count}
 	return render(request, 'accounts/customer.html', context)
+
+def createOrder(request, pk):
+
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10)
+	customer = Customer.objects.get(id=pk)
+	#form = OrderForm(initial={'customer':customer})
+	formset = OrderFormSet(queryset = Order.objects.none(), instance = customer)
+
+	if request.method == 'POST':
+		#print('Printing POST:', request.POST)
+		#form = OrderForm(request.POST, instance=form)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
+			return redirect('/')
+
+	context = {'formset': formset}
+
+	return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+
+	if request.method == 'POST':
+		#print('Printing POST:', request.POST)
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form': form}
+
+	return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+
+	if request.method == 'POST':
+		order.delete()
+		return redirect('/')
+
+	context = {'item':order}
+
+	return render(request, 'accounts/delete.html', context)
+
